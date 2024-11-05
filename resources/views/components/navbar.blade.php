4b38825ebfea3434bb7b690d574d199c199aa941
ridhann <ridhan783@gmail.com>
2024-11-06 01:19:31 +0700

    dashboard

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <title>Aktivara</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-light bg-blue fixed-top">
         <div class="container-fluid">
             <button type="button" id="sidebarCollapse" class="btn btn-primary">
                 <i class="fa-solid fa-bars"></i>
